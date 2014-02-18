e4e56f53e315d5891f01f54b4567add21d9e89bf
Felipe Theodoro <ftgoncalves@gmail.com>
2014-02-18 15:43:39 -0300

    alpha version

--- jquery.iscrollpage.js
+++ jquery.iscrollpage.js
@@ -1,32 +1,89 @@
 (function($) {
 
+    var mem = 0;
+    var isBlock = false;
+
     $.fn.iscrollpage = function(opt) {
         var settings = $.extend({
             'duration': 2000,
             'locationHash': true,
             'complete': function(){},
-            'start': function(){}
+            'start': function(){},
+            'always': function(){},
+            'element': "section"
         }, opt);
+
+        var _this = $(this);
+
+        var a = new Array();
+        var b = new Array();
+        $(settings.element).each(function(i, e){
+            var tmp = $(e);
+            a.push(tmp.offset().top);
+            b.push(tmp.attr("id"));
+        });
+
+        $(document).scroll(function() {
+            var _win = $(window);
+            var topView = _win.scrollTop();
+
+            if (topView > 0 && topView > mem && !isBlock) {
+                isBlock = true;
+
+                for (var i = 0; i <= a.length; i++) {
+
+                    if (a[i] > mem) {
+                        mem = a[i];
+                        _this._scroll(settings, mem, b[i]);
+                        break;
+                    }
+                }
+            }
+
+            if (topView > 0 && topView < mem && !isBlock) {
+                isBlock = true;
+                for (var i = (a.length - 1); i >= 0; i--) {
+
+                    if (a[i] < mem) {
+                        mem = a[i];
+                        _this._scroll(settings, mem, b[i]);
+                        break;
+                    }
+                }
+            }
+        });
 
         $(this).click(function(e) {
             e.preventDefault();
 
-            var _id = $(this).attr('href');
+            var _id = $(this).data('ref');
+            console.log(_id);
             var id = $(_id);
 
-            $("html,body").animate({
-                scrollTop: id.offset().top
-            }, {
-                duration: settings.duration,
-                start: function(){
-                    if (settings.locationHash)
-                        window.location.hash = _id;
-                },
-                complete: function() {
-                    settings.complete();
-                }
-            });
+            isBlock = true;
+            _this._scroll(settings, id, _id);
         });
     };
+
+    $.fn._scroll = function(settings, id, _id) {
+        $("html,body").animate({
+            scrollTop: (typeof id == 'object') ? id.offset().top : id
+        }, {
+            duration: settings.duration,
+            start: function() {
+                mem = (typeof id == 'object') ? id.offset().top : id;
+                console.log("f:" + mem);
+            },
+            complete: function() {
+                if (settings.locationHash)
+                    window.location.hash = _id;
+
+                settings.complete();
+            },
+            always: function() {
+                isBlock = false;
+            }
+        });
+    }
 
 }(jQuery));
